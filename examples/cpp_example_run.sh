4a102d5a50dbfbfdb13eaaaa4ab5ee2397b96d93
Jiayuan Mao <maojiayuan@gmail.com>
2021-07-20 16:52:08 -0400

    [master] macOS cpp example

--- examples/cpp_example_run.sh
+++ examples/cpp_example_run.sh
@@ -11,5 +11,8 @@ set -x
 CFLAGS="-std=c++14 -O2 $(pkg-config --cflags opencv)"
 LDFLAGS="$(pkg-config --libs opencv)"
 g++ $CFLAGS cpp_example.cpp -I../csrc/ -L../ -lpatchmatch $LDFLAGS -o cpp_example.exe
-LD_LIBRARY_PATH=../:$LD_LIBRARY_PATH time ./cpp_example.exe
+
+export DYLD_LIBRARY_PATH=../:$DYLD_LIBRARY_PATH  # For macOS
+export LD_LIBRARY_PATH=../:$LD_LIBRARY_PATH  # For Linux
+time ./cpp_example.exe
 
